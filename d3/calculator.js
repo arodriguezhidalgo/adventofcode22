354a1be9b7b39d352f9999b0e6e7c190a2c46069
Antonio Rodriguez-Hidalgo <antonior@mathworks.com>
2022-12-11 23:07:30 +0000

    d3. Second half done

--- d3/calculator.js
+++ d3/calculator.js
@@ -2,6 +2,7 @@
 // We need to wait for the result of the fetch call, otherwise we cannot store the content in a variable (loading time is slower, so we get to textContent too quickly).
 // Consequently, wrap everything in an async function and then wait for the promises to finish. Then, return the variable.
 
+// For the second half, group in triplets and then repeat all the xor logic
 async function getTextContent(){
     // See https://fetch.spec.whatwg.org/
     /* Fetch (specifically, request) the document. 
@@ -19,32 +20,28 @@ let textContent;
 // Retrieve the text content
 await getTextContent();
 
-//let splitText = textContent.map(text => text.split(" "));
-
-
-
-function oneHotEncodeCharacter(inputHalf){
-    // Create an empty vector with zeros. 26 characters for the abc..., twice.
-    let encodedHalf = new Array(26*2).fill(0);
-    // 65 is the unicode value of "A"
-    inputHalf.map(character => character.charCodeAt(0) - 65)
-                           .map(encodedCharacter => encodedHalf[encodedCharacter] = 1);
-
-    return encodedHalf;
+let triplets = [];
+for (let i=0; i < textContent.length; i += 3){
+    const contatenatedStrings = [textContent[i], textContent[i+1],textContent[i+2]];
+    triplets.push(contatenatedStrings);    
 }
 
-let decodedText = textContent.map(x => {
+
+let decodedText = triplets.map(x => {
         let duplicateItems;
         const len = x.length;
+        
         // Slice the vector in two halves. Then, use split("") to convert the string in an Array of chars
-        const firstHalf = x.slice(0, len/2).split("");
-        const secondHalf = x.slice(len/2, len).split("");
+        const firstHalf = x[0].split("");
+        const secondHalf = x[1].split("");
+        const thirdHalf = x[2].split("");
 
         let firstEncodedVector = oneHotEncodeCharacter(firstHalf);
         let secondEncodedVector = oneHotEncodeCharacter(secondHalf);
+        let thirdEncodedVector = oneHotEncodeCharacter(thirdHalf);
 
         for (let i=0; i<firstEncodedVector.length; i++){
-            if ((firstEncodedVector[i] == secondEncodedVector[i]) && (secondEncodedVector[i] == 1)){
+            if ((firstEncodedVector[i] == secondEncodedVector[i]) && (secondEncodedVector[i] == 1) && (firstEncodedVector[i] == thirdEncodedVector[i]) && (thirdEncodedVector[i] == 1) && (secondEncodedVector[i] == thirdEncodedVector[i]) && (thirdEncodedVector[i] == 1)){
                 // Find items that are the same in the two vectors.
                 duplicateItems = i;
                 }
@@ -55,14 +52,24 @@ let decodedText = textContent.map(x => {
         .reduce((x,y) => x+y);
 
 
+
+function oneHotEncodeCharacter(inputHalf){
+    // Create an empty vector with zeros. 26 characters for the abc..., twice.
+    let encodedHalf = new Array(26*2).fill(0);
+    // 65 is the unicode value of "A"
+    inputHalf.map(character => character.charCodeAt(0) - 65)
+                            .map(encodedCharacter => encodedHalf[encodedCharacter] = 1);
+
+    return encodedHalf;
+}
+        
 function decode(x){
     // "z" has code 122
     // "a" has code 97
     // "A" has code 65
 
     // First, add 65 to recover the char code
-    x = x + 65;
-    console.log(String.fromCharCode(x));
+    x = x + 65;    
     // Then, check if its capital letter or not
     if (x >= 97){
         // Lower case
@@ -70,26 +77,9 @@ function decode(x){
     }
     else{
         return x-65+27;
-    }
-    
+    }    
 }
 
-debugger
 
-function compareOptions(player1, player2){
-    const mappedPlayer1 = mappingPlayer1[player1];
-    const mappedPlayer2 = mappingPlayer2[player2];
-    
-    return (scores[mappedPlayer1][mappedPlayer2] + fixedScores[mappedPlayer1][mappedPlayer2]);    
-}
 
-let output = textContent.map(text => 
-    {
-        const playerChoices = text.split(" ");
-        const player1 = playerChoices[0];
-        const player2 = playerChoices[1];
-        return compareOptions(player1, player2);
-    })
-    .reduce((a,b) => a+b);
-
-document.getElementById("Total").innerText = output;
+document.getElementById("Total").innerText = decodedText;
